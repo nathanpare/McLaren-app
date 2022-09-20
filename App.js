32aa30557491f94d3abc00656b71eb8f4796cf52
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-09-20 16:38:32 -0700

    Create main component

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello from React Native!</Text>
+      <View style={styles.carContainer}>
+        <ImageBackground
+         source={require("./assets/p1.jpeg")}
+         style={styles.image}
+         />
+      <View style={styles.titles}>
+        <Text style={styles.title}>McLaren P1</Text>
+        <Text style={styles.subtitle}>starting at $1,150,000</Text>
+      </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +26,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#5c5e62"
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: 'absolute'
+  }
 });
